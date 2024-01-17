5e84dd79ddeed1ecb829da9997202c3ba63af948
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-01-17 18:49:15 +0100

    built in router system

--- next.js-tutorial/src/app/page.js
+++ next.js-tutorial/src/app/page.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      Home Page
+      <ul>
+        <li>
+          {" "}
+          <Link href="/users">Users</Link>
+        </li>
+        <li>
+          <Link href="/users/new">New User</Link>
+        </li>
+      </ul>
+    </main>
+  );
 }
