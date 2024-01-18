00a4436538111ea865dd4aa48a970b190fb599e4
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-01-18 14:47:00 +0100

    server side data fetching

--- next.js-tutorial/src/app/users/page.jsx
+++ next.js-tutorial/src/app/users/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>usersPage</div>;
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>UsersPage</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
