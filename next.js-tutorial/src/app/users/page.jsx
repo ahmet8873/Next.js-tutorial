b743c50eb589bacaa82932da0dfb556d690a5a04
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-01-18 16:57:58 +0100

    dynamic and static server side rendering

--- next.js-tutorial/src/app/users/page.jsx
+++ next.js-tutorial/src/app/users/page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const data = await res.json();
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  // cache: "no-store" will not store the data in the cache, dynamically fetching the data at every request
   const data = await res.json();
 
   return (
