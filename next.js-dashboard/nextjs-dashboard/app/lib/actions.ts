cc1914f3022e81c23453f015abc9e7b15cff7cfe
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-02-08 15:39:12 +0100

    authentication and authorisation

--- next.js-dashboard/nextjs-dashboard/app/lib/actions.ts
+++ next.js-dashboard/nextjs-dashboard/app/lib/actions.ts
@@ -7,6 +7,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -61,4 +64,24 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+// ------------
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
